e05e2524ddf3e28367aec85fe5d8dc075564d851
tbowman01 <tbowman01@users.noreply.github.com>
2025-07-29 15:29:22 +0000

    chore: Remove Docusaurus generated files and update .gitignore
    - Add docs-site/.docusaurus/ to .gitignore to prevent tracking generated files
    - Remove all .docusaurus generated files from git tracking
    - This prevents merge conflicts with auto-generated content

--- docs-site/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/workspaces/PhotonDrift/docs-site/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/PhotonDrift/docs-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/PhotonDrift/docs-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/PhotonDrift/docs-site/src/css/custom.css"),
-  require("/workspaces/PhotonDrift/docs-site/node_modules/plugin-image-zoom/src/zoom"),
-];
